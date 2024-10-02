d8431c47e386a736c2402b792e85c75926b55992
wongakim-99 <202021000@sangmyung.kr>
2024-10-02 10:16:04 +0900

    #4 - Websocket 컨트롤러 수정
    앞서 만든 컨트롤러, 서비스, DTO 로직을 반영하여 웹소켓 간단하게 구현

--- chatting/chatting/src/main/java/com/websocket/chatting/handler/WebsockHandler.java
+++ chatting/chatting/src/main/java/com/websocket/chatting/handler/WebsockHandler.java
@@ -1,5 +1,10 @@
 package com.websocket.chatting.handler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.websocket.chatting.dto.ChatMessage;
+import com.websocket.chatting.dto.ChatRoom;
+import com.websocket.chatting.service.ChatService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.TextMessage;
@@ -7,14 +12,22 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 @Slf4j
+@RequiredArgsConstructor
 @Component
 public class WebsockHandler extends TextWebSocketHandler {
+    private final ObjectMapper objectMapper;
+    private final ChatService chatService;
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String payload = message.getPayload();
         log.info("payload {}", payload);
-        TextMessage textMessage = new TextMessage("Welcome chatting server!");
-        session.sendMessage(textMessage);
+
+        //TextMessage textMessage = new TextMessage("Welcome chatting server!");
+        //session.sendMessage(textMessage);
+
+        ChatMessage chatMessage = objectMapper.readValue(payload, ChatMessage.class);
+        ChatRoom room = chatService.findRoomById(chatMessage.getRoomId());
+        room.handleActions(session, chatMessage, chatService);
     }
 }
