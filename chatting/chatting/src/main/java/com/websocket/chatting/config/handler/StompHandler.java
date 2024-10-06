971771f0bb8d0ef599d753a751bfea176cc51226
wongakim-99 <202021000@sangmyung.kr>
2024-10-06 17:18:23 +0900

    #7 - StompHandler 클래스 코드 주석 몇 줄 작성
    StompHandler 클래스에 대한 상세한 설명은 이슈번호 7번의 마크다운 StompHandler 부분 참고

--- chatting/chatting/src/main/java/com/websocket/chatting/config/handler/StompHandler.java
+++ chatting/chatting/src/main/java/com/websocket/chatting/config/handler/StompHandler.java
@@ -18,6 +18,8 @@ public class StompHandler implements ChannelInterceptor {
     private final JwtTokenProvider jwtTokenProvider;
 
     // websocket을 통해 들어온 요청이 처리 되기전 실행된다.
+    // presend 메서드 같은 경우 WebSocket 메시지가 실제로 전송되기 전에 호출
+    // 메시지의 헤더와 내용 등을 검사하고 필요한 처리를 할 수 있는 곳
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
